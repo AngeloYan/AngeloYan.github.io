40bce7267e7dd45b591c27aab5a5d17b59e2c49a
Angelo Yan <angeloyanemail@gmail.com>
2019-06-02 19:43:29 +1000

    set up functions to load projects

---
+++ public/js/main.js
@@ -0,0 +1,9 @@
+function addProject(img, alt, title, description, featuresArray, youtubeLink, projectLink) {
+    console.log("hello");
+}
+
+function loadProjects() {
+    console.log("loading");
+}
+
+window.onload = loadProjects();
